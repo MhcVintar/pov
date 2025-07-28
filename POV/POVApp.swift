4d96b71db185d438f7fb1c664534699833b6385d
MhcVintar <mihavintar3@gmail.com>
2025-07-28 21:19:02 +0200

    Initial Commit

---
+++ POV/POVApp.swift
@@ -0,0 +1,17 @@
+//
+//  POVApp.swift
+//  POV
+//
+//  Created by Miha Vintar on 28. 7. 25.
+//
+
+import SwiftUI
+
+@main
+struct POVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
